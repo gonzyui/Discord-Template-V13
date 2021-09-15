9cc0b0782eb51108d374bf66d443385a0639d81a
gonzyui <tomarauch15@gmail.com>
2021-09-15 23:58:04 +0200

    Created prefix command

---
+++ src/commands/admin/prefix.js
@@ -0,0 +1,22 @@
+module.exports = class Prefix extends Command {
+    constructor() {
+        super({
+            name: "prefix",
+            aliases: ["pref"],
+            description: "Prefix command",
+            usage: "<prefix>",
+            category: "Admin",
+            ownerOnly: false,
+            cooldown: 3000,
+            memberPerms: ["MANAGE_GUILD"],
+            clientPerms: [],
+        });
+    }
+    async exec(message, [prefix], data) {
+        if (!prefix) return message.reply(`Actual prefix is ${data.guild?.prefix}`);
+        if (prefix.length > 3) message.reply(`The prefix cannot be longer than 3 characters !`);
+        data.guild.prefix = prefix;
+        await data.guild.save();
+        return message.reply(`Changed prefix to ${prefix}`);
+    }
+}
