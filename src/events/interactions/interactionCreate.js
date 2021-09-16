02d3d10dab45bd4670ca180d9f8af163c3175669
gonzyui <tomarauch15@gmail.com>
2021-09-16 02:33:35 +0200

    Updated for context commands

--- src/events/interactions/interactionCreate.js
+++ src/events/interactions/interactionCreate.js
@@ -8,6 +8,9 @@ module.exports = class interactionCreate extends Event {
     async exec(interaction) {
         const data = {};
         data.guild = await this.client.findGuild({ guildID: interaction.guildId });
+        /* Slash commands */
         if (interaction.isCommand()) return this.client.emit('slashCommands', interaction, data);
+        /* User commands (when right click on an username) */
+        if (interaction.isContextMenu()) return this.client.emit('slashCommands', interaction, data);
     }
 }
