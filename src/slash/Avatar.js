64108c14d3ffee5dd5e4f4dae17bbf83165a191a
gonzyui <tomarauch15@gmail.com>
2021-09-16 02:33:18 +0200

    Created context cmd Avatar

---
+++ src/slash/Avatar.js
@@ -0,0 +1,15 @@
+const { MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports = class Avatar extends Interaction {
+    constructor() {
+        super({
+            name: "Avatar",
+            type: 'USER',
+            description: 'Gets Avatar'
+        });
+    }
+    async exec(interaction) {
+        const row = new MessageActionRow().addComponents(new MessageButton().setStyle('LINK').setURL(interaction.options.getUser('user').displayAvatarURL({ dynamic: true, size: 4096 })).setLabel("Show"))
+        interaction.reply({ content: `${interaction.options.getUser('user').displayAvatarURL({ dynamic: true, size: 4096 })}`, components: [row] });
+    }
+}
