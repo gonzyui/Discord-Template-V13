d261bb48c1f9e38eea448c5a86a44a9b612abf5a
gonzyui <tomarauch15@gmail.com>
2021-09-21 04:20:25 +0200

    added welcome channel value

--- src/models/Guilds.js
+++ src/models/Guilds.js
@@ -4,4 +4,5 @@ module.exports = model('Guilds', new Schema({
     guildID: { type: String },
     prefix: { type: String, default: '!' },
     language: { type: String, default: 'en' },
+    welcomeChannel: { type: String, default: '874220241880764506' },
 }))
